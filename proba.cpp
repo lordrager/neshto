e118051c24689576208983faa683997a322ab448
lordrager <35623157+lordrager@users.noreply.github.com>
2018-01-20 11:54:35 +0100

    Add files via upload

---
+++ proba.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{int n;
+cout<<"Vavedi cislo:";
+cin>>n;
+int a[n];
+cout<<"Vavedete cislata si :";
+for(int i=0;i<=n;i++){
+cin>>a[i];cout<<endl;}
+
+system("pause");
+return 0;
+}
